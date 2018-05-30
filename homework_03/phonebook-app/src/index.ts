e9346af207ba2b16d117b7973d85ce9b6b550c67
Kristian Angelski <kristian.angelski@gmail.com>
2018-05-30 12:49:24 +0300

    added tsconfig, webpack.config, index.ts and test index.ts. files

---
+++ homework_03/phonebook-app/src/index.ts
@@ -0,0 +1,8 @@
+namespace app {
+
+  export const init = ():void => {
+    console.log('app started');
+  }
+}
+
+app.init();
---
+++ homework_03/phonebook-app/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: './dist/app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      loader: 'ts-loader'
+    }]
+  }
+};
